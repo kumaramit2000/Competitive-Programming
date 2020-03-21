59780cd8a191361f30206726fcddf2e6f41766a6
kumaramit2000 <47832506+kumaramit2000@users.noreply.github.com>
2020-03-21 15:51:01 +0530

    Update count_the_tripletes.cpp

--- Arrays/count_the_tripletes.cpp
+++ Arrays/count_the_tripletes.cpp
@@ -35,29 +35,3 @@ ll findtriplets(ll a[],ll n)
     }
     return sum;
 }
-using namespace std;
-int main() 
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    ll t;
-    cin>>t;
-    while(t--)
-    {
-        ll n;
-        cin>>n;
-        ll a[n];
-        for(int i=0;i<n;i++)
-        {
-            cin>>a[i];
-        }
-        sort(a,a+n);
-        ll ans=findtriplets(a,n);
-        if(ans==0)
-        {
-            ans=-1;
-        }
-        cout<<ans<<endl;
-    }
-    return 0;
-}
