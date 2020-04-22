5690e6bb65f2425bf556ee8eaa2e48135260b06b
kumaramit2000 <47832506+kumaramit2000@users.noreply.github.com>
2020-04-22 07:59:49 +0530

    Update rotate_a_linked_list.cpp

--- Linked List/rotate_a_linked_list.cpp
+++ Linked List/rotate_a_linked_list.cpp
@@ -1,5 +1,6 @@
 /*
-
+Given a singly linked list of size N. The task is to rotate the linked list counter-clockwise by k nodes, where k 
+is a given positive integer smaller than or equal to length of the linked list.
 */
 /*---------------------------1st way----------------------------*/
 // { Driver Code Starts
