fc91ea24d700f0e1ebff0cb7390f3bc05de00688
kumaramit2000 <47832506+kumaramit2000@users.noreply.github.com>
2020-04-23 00:40:13 +0530

    Create flattening_a_linked_list.cpp

---
+++ Linked List/flattening_a_linked_list.cpp
@@ -0,0 +1,24 @@
+/*
+Given a Linked List of size N, where every node represents a linked list and contains two pointers of its type:
+(i) a next pointer to the next node,
+(ii) a bottom pointer to a linked list where this node is head.
+
+You have to flatten the linked list to a single linked list which should be sorted.
+       5 -> 10 -> 19 -> 28
+       |    |     |     |
+       V    V     V     V
+       7    20    22    35
+       |          |     |
+       V          V     V
+       8          50    40
+       |                |
+       V                V
+       30               45
+       
+And after flattening the above list, the sorted linked list looks like:
+
+ 5-> 7-> 8- > 10 -> 19-> 20-> 22-> 28-> 30-> 35-> 40-> 45-> 50.
+
+Note: The flattened list will be printed using the bottom pointer instead of next pointer.       
+*/
+
