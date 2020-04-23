b4dd9821f3c2110f7c1b7a323e8dd4477e748c0f
kumaramit2000 <47832506+kumaramit2000@users.noreply.github.com>
2020-04-23 10:32:06 +0530

    Create pairwise_swap.cpp

---
+++ Linked List/pairwise_swap.cpp
@@ -0,0 +1,99 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *pairWiseSwap(Node *head);
+
+void insert(Node **head) {
+    int n, i, value;
+    Node *temp;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        if (i == 0) {
+            *head = new Node(value);
+            temp = *head;
+        } else {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+void printList(struct Node *temp) {
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+int main(void) {
+    /* Start with the empty list */
+    int t, k, n, value;
+
+    /* Created Linked list is 1->2->3->4->5->6->7->8->9 */
+    scanf("%d", &t);
+
+    Node *head = NULL;
+
+    while (t--) {
+        head = NULL;
+        insert(&head);
+        head = pairWiseSwap(head);
+        printList(head);
+    }
+    return (0);
+}
+// } Driver Code Ends
+/*
+  Pairwise swap a linked list
+  The input list will have at least one element
+  node is defined as
+
+struct node
+{
+    int data;
+    struct node* next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}*head;
+*/
+Node* pairWiseSwap(struct Node* head) {
+    Node* temp=head;
+    if(head==NULL)
+    {
+        return head;
+    }
+    Node* f=head;
+    Node* s=head->next;
+    while(f&&s)
+    {
+        int temp=f->data;
+        f->data=s->data;
+        s->data=temp;
+        f=s->next;
+        if(f)
+        s=f->next;
+    }
+    return head;
+}
