457dc937bec0a9967d6433cf81ee68cc5b2d93d0
kumaramit2000 <47832506+kumaramit2000@users.noreply.github.com>
2020-04-24 21:52:27 +0530

    Create next_largest_element.cpp

---
+++ Stack and Queue/next_largest_element.cpp
@@ -0,0 +1,55 @@
+/*love_for_code*/
+/*code_for_love*/
+//Code_written_by: @AMIT KUMAR;
+#define ll long long int
+#define ull unsigned long long int
+#define pb push_back
+#define pob pop_back
+#define MODM 1000000007
+#define MODS 10000007
+#define INF 2000000000000000000LL
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        ll dp[n];
+        stack<ll> s;
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!s.empty()&&a[i]>=s.top())
+            {
+                s.pop();
+            }
+            ll sz=s.size();
+            if(sz==0)
+            {
+                dp[i]=-1;
+            }
+            else
+            {
+                dp[i]=s.top();
+            }
+            s.push(a[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<dp[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
